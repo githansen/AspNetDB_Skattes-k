1c2fd1d54af40ab3526cdd7c56b8e6c1188d9a8e
githansen <yohanhansen97@gmail.com>
2022-12-25 17:19:41 +0100

    v

--- AspNetDB_Skattesøk/Program.cs
+++ AspNetDB_Skattesøk/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
             .AllowAnyMethod();
         });
 });
-builder.Services.AddDbContext<Database>(options => options.UseSqlServer("Server=localhost;Database=Person;Trusted_Connection=True;"));
+builder.Services.AddDbContext<Database>(options => options.UseSqlServer("Server=tcp:serverdbtestbach.database.windows.net,1433;Initial Catalog=TestDatabase;Persist Security Info=False;User ID=s349880@oslomet.no;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Authentication=\"Active Directory Integrated\";"));
 
 
 var app = builder.Build();
