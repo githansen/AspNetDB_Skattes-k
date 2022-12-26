65441cbf967142bdf465b629fb307429763cf6c1
githansen <yohanhansen97@gmail.com>
2022-12-26 01:02:07 +0100

    v

--- AspNetDB_Skattesøk/Program.cs
+++ AspNetDB_Skattesøk/Program.cs
@@ -17,11 +17,11 @@ builder.Services.AddCors(options =>
             .AllowAnyMethod();
         });
 });
-//builder.Services.AddDbContext<Database>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddDbContext<Database>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 
 var app = builder.Build();
-//DBinit.Initialize(app);
+DBinit.Initialize(app);
 
 
 // Configure the HTTP request pipeline.
