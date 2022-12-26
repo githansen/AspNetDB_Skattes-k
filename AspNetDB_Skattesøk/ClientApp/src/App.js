d25788de2b4d9c31fc48bb913c2a3229f8a37c3b
githansen <yohanhansen97@gmail.com>
2022-12-26 12:03:59 +0100

    v

--- AspNetDB_Skattesøk/ClientApp/src/App.js
+++ AspNetDB_Skattesøk/ClientApp/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-
+import $ from 'jquery'
 import './custom.css';
 
+function test() {
+    $.get("/https://skattjohan.azurewebsites.net/api/Home/retur", function (data) {
+        console.log(data)
+    })
+}
 function App() {
     return (
         <div>
             hei
+            <button onClick={test }>Test</button>
+            <div id="ut"></div>
         </div>
     )
 }
